5b5f3b694cdb0ded0b3f332c9956e8248e462265
reinaldo agostinho <reinaldoasf@gmail.com>
2019-04-18 18:18:55 -0300

    a differet gap pi formation

--- trap_gap8.c
+++ trap_gap8.c
@@ -1,4 +1,4 @@
-//* Compile: gcc -g -Wall -fopenmp -o omp_trap3 omp_trap3.c
+//* Compile: gcc -g -Wall -fopenmp -o omp_trap2 omp_trap3.c
 #include "cmsis.h"
 #include <math.h>
 #include "gap_common.h"
@@ -24,7 +24,8 @@ typedef struct integral
 	int a;
 	int b;
 	int n;
-	int thread_count;
+	int soma;
+
 }integral;
 
 uint32_t current_voltage(void)
@@ -42,23 +43,23 @@ int f(int x) {
 }  /* f */
 
 
-int approx;
+int soma_global=0;
+
 void Trap(void* arg){
+	int approx;
    int  i;
    int h;
-	integral* prov = (integral*) arg;
-    h = (prov[__core_ID()].b-prov[__core_ID()].a)/prov[__core_ID()].n; 
-   //printf("o valor de a = %d, o valor de b = %d, o valor de n = %d no core %d",&prov[__core_ID()]->a,&prov[__core_ID()]->b,&prov[__core_ID()]->n,__core_ID()); 
-   approx = (f(prov[__core_ID()].a) + f(prov[__core_ID()].b))/2.0; 
-//#  pragma omp parallel for num_threads(thread_count)      reduction(+: approx)
-	EU_MutexLock(0);
-   for (i = 1; i <= prov[__core_ID()].n-1; i++)
-     approx += f(prov[__core_ID()].a + i*h);
-	EU_MutexUnlock(0);
-   approx = h*approx; 
+	integral** prov=(integral**)arg;
+    h = (prov[__core_ID()]->b-prov[__core_ID()]->a)/prov[__core_ID()]->n; 
+			printf("%d\n",prov[__core_ID()]->b);
+approx = (f(prov[__core_ID()]->a) + f(prov[__core_ID()]->b))/2; 
+		 for (i=1;i<prov[__core_ID()]->n;i++){
+		 	approx += f(prov[__core_ID()]->a + i*h);
+		 }
+		 approx = h*approx;
 
-//   return approx;
-}  /* Trap */
+soma_global+=approx;
+}  
 void Master_Entry(void* arg) 
 {
 	CLUSTER_CoresFork(Trap, arg);
@@ -67,31 +68,34 @@ void Master_Entry(void* arg)
 
 int main()
 {
-	   int  global_result = 0.0; /* Store result in global_result */
-   int a=0, b=1000;                 /* Left and right endpoints*/
-   int     n=20;                    /* Total number of trapezoid*/
-   int     thread_count=CORE_NUMBER;
-	integral* integ=L1_Malloc(CORE_NUMBER*sizeof(struct integral));
+	int  global_result = 0; /* Store result in global_result */
+	int a=0, b=800;                 /* Left and right endpoints*/
+	int     n=80;                    /* Total number of trapezoid*/
+	int     thread_count=CORE_NUMBER;
 
-	integ->a=a;
-	integ->b=b;
-	printf("o valor de b e: %d\n\n",&integ->b);
-	integ->n=n;
-	integ->a=a;
-	CLUSTER_Start(0, CORE_NUMBER);
+	integral** integ=L1_Malloc(CORE_NUMBER*sizeof(integral));
+	int h = (b-a)/n;
+	printf("o valor de eh, %d",h);
+	for(int i=0;i<thread_count;i++)
+	{
+		integ[i]->soma=0;
+		integ[i]->a = a +h*i;
+		integ[i]->n = n;
+		integ[i]->b = a+h*(i+1);
+		printf("os valores do cluster %d sao a = %d, b = %d, n = %d\n",i,integ[i]->a,integ[i]->b,integ[i]->n);
+	}                                                                    
+	CLUSTER_Start(0, CORE_NUMBER);                                       
 
     if (FLL_SetFrequency(uFLL_CLUSTER, CLUSTER_FREQ, 0) == -1) {
         printf("Error of changing frequency, check Voltage value!\n");
     }
 	CLUSTER_SendTask(0, Master_Entry,(void*) integ, 0);
     printf("Waiting...\n");
-
+	
 
 	CLUSTER_Wait(0);
 
-
-   printf("With n = %d trapezoids, our estimate\n", integ->n);
-   printf("of the integral from %d to %d = \n",
-      integ->a, integ->b, approx);
+   printf("With n = %d trapezoids, our estimate\n", n);
+   printf("of the integral from %d to %d = \n",a,b,soma_global);
    return 0;
 }
